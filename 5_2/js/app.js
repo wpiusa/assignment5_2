a41b22b880396de866ca5b4a79e18967bf24ec80
wpiusa <wpiusa@gmail.com>
2017-03-08 14:30:30 -0800

    Add files via upload

---
+++ 5_2/js/app.js
@@ -0,0 +1,13 @@
+
+var person={
+    name: "Tom Lee",
+    age:52,
+    dob:"07/11/106",
+    placeOfBirth:"Korea"	
+}
+
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.dob);
+console.log(person.placeOfBirth);
